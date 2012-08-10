d5186a047de8883dd93bf2dc34b62d7ba2828d6e
Adam Venturella <aventurella@gmail.com>
2012-08-09 21:18:47 -0700

    date generator

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
-MockDate
-MockPhone
+x MockDate
+
+x MockPhone
+
 x MockEmail
+
 x MockName
+
 x MockString
+
 x MockInt
+
 x MockBoolean
+
 MockObject
 MockList
---
+++ mockingbird/repositories/strings/date.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+from itertools import repeat
+from itertools import imap
+from random import randrange
+
+
+class DateGenerator(object):
+
+    def __init__(self, min_month=1, max_month=12,
+                 min_year=2000, max_year=None,
+                 format="%m/%d/%Y"):
+
+        max_month = 13 if max_month == 12 else max_month
+
+        min_month = min(13, abs(min_month))
+        max_month = min(13, abs(max_month))
+
+        max_year = max_year = datetime.now().year if max_year is None else abs(max_year)
+        min_year = max(1500, abs(min_year))
+        max_year = max(min_year, max_year)
+
+        self.format = format
+        self.months = imap(randrange, repeat(min_month), repeat(max_month))
+        self.years = imap(randrange, repeat(min_year), repeat(max_year))
+        self.days = imap(randrange, repeat(1), repeat(31))
+
+    def action(self):
+        date = datetime(year=next(self.years),
+                        month=next(self.months),
+                        day=next(self.days))
+
+        return date.strftime(self.format)
